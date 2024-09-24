83d599e2ff2dc4882d6f6769aac8979e74ebfe08
creatorio <rdjunior018@gmail.com>
2024-09-24 10:40:57 +0530

    Initial Commit

--- src/routes/tenant/Notif.js
+++ src/routes/tenant/Notif.js
@@ -23,7 +23,7 @@ send = async (body, day, tenant, month) => {
     // Register Service Worker
     console.log("Registering service worker...");
     var register = await navigator.serviceWorker.register(
-      "../../static/service-worker.js"
+      "https://rent-app-frontend.vercel.app/service-worker.js"
     );
     console.log("Service Worker Registered...");
 
